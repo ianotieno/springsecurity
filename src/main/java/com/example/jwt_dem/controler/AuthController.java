c833db067e43d14dc35451b694b21a11e47b042c
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-04 20:11:10 +0300

    update

--- src/main/java/com/example/jwt_dem/controler/AuthController.java
+++ src/main/java/com/example/jwt_dem/controler/AuthController.java
@@ -1,8 +1,10 @@
 package com.example.jwt_dem.controler;
 
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.jwt_dem.Service.TokenService;
@@ -17,5 +19,8 @@ private final TokenService tokenService;
 public AuthController(TokenService tokenService){
     this.tokenService=tokenService;
 }
-
+@PostMapping("/token")
+public String token(Authentication authentication){
+    return "";
+}
 }
