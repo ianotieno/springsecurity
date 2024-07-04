f45dc2f4d793646dfdc9fb6aa55d1a551151a8f1
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-04 18:09:23 +0300

    update

---
+++ src/main/java/com/example/jwt_dem/JwtDemApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_dem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt_dem/JwtDemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_dem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
