f0f80f15024d7607d611b6448633065cb1ad8a27
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-04 19:07:00 +0300

    rsakey

--- src/main/java/com/example/jwt_dem/config/SecurityConfig.java
+++ src/main/java/com/example/jwt_dem/config/SecurityConfig.java
@@ -8,12 +8,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	private final RsaKeyProperties rsaKeyProperties;
+    public SecurityConfig(RsaKeyProperties rsaKeyProperties){
+		this.rsaKeyProperties=rsaKeyProperties;
+	}
+	
 	@Bean
 	public InMemoryUserDetailsManager user() {
 		return new InMemoryUserDetailsManager(User.withUsername("ian")
@@ -35,6 +43,9 @@ public class SecurityConfig {
 				.httpBasic(Customizer.withDefaults())
 				.build();
 	}
-	
+	@Bean
+	JwtDecoder JwtDecoder(){
+		return NimbusJwtDecoder.withPublicKey(rsaKeyProperties.publicKey()).build();
+	}
 
 }
