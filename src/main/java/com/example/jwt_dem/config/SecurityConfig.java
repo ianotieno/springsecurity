ffb3487bfbd187c61741a679e8419b7b6f95b322
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-04 19:18:58 +0300

    encoder

--- src/main/java/com/example/jwt_dem/config/SecurityConfig.java
+++ src/main/java/com/example/jwt_dem/config/SecurityConfig.java
@@ -9,9 +9,17 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 @EnableWebSecurity
@@ -47,5 +55,11 @@ public class SecurityConfig {
 	JwtDecoder JwtDecoder(){
 		return NimbusJwtDecoder.withPublicKey(rsaKeyProperties.publicKey()).build();
 	}
+@Bean
+JwtEncoder jwtEncoder(){
 
+	RSAKey jwk= new RSAKey.Builder(rsaKeyProperties.publicKey()).privateKey(rsaKeyProperties.privateKey()).build();
+	JWKSource<SecurityContext> jwks= new ImmutableJWKSet<>(new JWKSet(jwk));
+	return new NimbusJwtEncoder(jwks);
+}
 }
